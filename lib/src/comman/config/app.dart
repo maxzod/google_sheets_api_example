d5bfd4cdf5cd508c2ac4465d6825a3864394e06c
Ahmed Masoud <maxzod66@gmail.com>
2021-08-24 21:07:53 +0200

    set up config

---
+++ lib/src/comman/config/app.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/foundation.dart';
+
+class AppConfig {
+  AppConfig._();
+  static const enableLogs = kDebugMode;
+  static const splashImage = 'assets/images/splash.png';
+  static const appName = 'Cyborgs Tech';
+}
